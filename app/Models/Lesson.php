d106da7be13e360cb5edca67d2253f218fddd13f
walter <walterrjr.86@gmail.com>
2022-10-17 11:30:52 -0300

    aula-3C-ok

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -5,19 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Lesson extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'description',
-        'image',
-        'available'
+       'name',
+       'description',
+       'url',
+       'video'
     ];
 
     public function modules()
     {
-        return $this->hasMany(Module::class);
+        return $this->belongsTo(Module::class);
     }
+
 }
