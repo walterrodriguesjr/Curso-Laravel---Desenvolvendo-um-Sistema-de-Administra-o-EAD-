c104883c36b09db213aa95e21978ac1c26ba687f
walter <walterrjr.86@gmail.com>
2022-11-16 17:41:30 -0300

    Method App\Http\Controllers\Admin\CourseController::edit does not exist.

---
+++ app/Http/Requests/Course/StoreCourse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCourse extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'max:255',
+                'unique:courses'
+            ],
+            'image' => [
+                'nullable',
+                'image',
+                'max:1024',
+            ],
+            'description' => [
+                'nullable',
+                'min:3',
+                'max:9999',
+            ],
+            'available' => [
+                'nullable',
+                'boolean',
+            ],
+        ];
+    }
+}
+
