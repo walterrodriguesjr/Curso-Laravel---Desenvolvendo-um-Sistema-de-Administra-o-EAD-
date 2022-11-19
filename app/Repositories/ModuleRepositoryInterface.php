59005e66670a2ea20d7335dcb57e67e49e0ffe8f
walter <walterrjr.86@gmail.com>
2022-11-18 21:07:21 -0300

    aula-29G-ok

--- app/Repositories/ModuleRepositoryInterface.php
+++ app/Repositories/ModuleRepositoryInterface.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 interface ModuleRepositoryInterface
 {
-    public function getAllByCourseId(string $courseId, string $filter = ''): array;
+    public function getAllByCourseId(string $courseId, string $filter = '');
     public function findById(string $id): ?object;
     public function createByCourse(string $courseId, array $data): object;
     public function update(string $id, array $data): ?object;
